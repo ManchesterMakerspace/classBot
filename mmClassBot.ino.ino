3af00e92eb09fe29052b8066772cd5e4bdda121e
Paul Beaudet <inof8or@gmail.com>
2016-11-04 22:25:15 -0400

    Illustarate examples a bit better

--- mmClassBot.ino.ino
+++ mmClassBot.ino.ino
@@ -3,22 +3,34 @@
 // Compiled with Arduino IDE 1.6.11
 
 // dependancies: Call libraries
-#include <JS_Timer.h>        // https://github.com/PaulBeaudet/JS_Timer
-JS_Timer timer = JS_Timer(); // delay can be used, but a timer is better for multi-tasking
+#include <JS_Timer.h>             // https://github.com/PaulBeaudet/JS_Timer
+#include <Adafruit_MotorShield.h> // https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
+
 // Set up constant numbers
-#define LED            13    // for debuging durring run time (pin 13)
-#define MONITOR        0     // param for a polymorphic fuction that has a monitor mode
-#define TRIGGER        1     // truth parameter = true || highs
-#define OFFSET         13    // hard coded offset of the motors set this proper to drive straight
-#define TEST_DURRATION 500   // durration in milliseconds
-#define MAX_POWER      255   // max drive speed
+#define LED            13   // for debuging durring run time (pin 13)
+#define MONITOR        0    // param for a polymorphic fuction that has a monitor mode
+#define TRIGGER        1    // truth parameter = true || highs
+#define OFFSET         13   // hard coded offset of the motors set this proper to drive straight
+#define TEST_DURRATION 80   // durration in milliseconds
+#define MAX_POWER      255  // max drive speed
+#define LEFT_MOTOR_PORT 3   // motor number on Adafruit shield
+#define RIGHT_MOTOR_PORT 4
+
+// create library objects
+JS_Timer timer = JS_Timer(); // delay can be used, but a timer is better for multi-tasking
+Adafruit_MotorShield motorShield = Adafruit_MotorShield(); // Create motor shield object w/ default I2C address
+Adafruit_DCMotor *leftMotor = motorShield.getMotor(LEFT_MOTOR_PORT);
+Adafruit_DCMotor *rightMotor = motorShield.getMotor(RIGHT_MOTOR_PORT);
 
 // ----- Main Arduino functions ---------
 
 void setup() {          // put your setup code here, to run once:
-  Serial.begin(115200); // trun up Serial to over 9600 bps!
+  Serial.begin(115200); // turn up Serial to over 9600 bps!
   motorSetup();         // instantiate motors
-  // forwardTest();       // test going forward for Test durration
+  // forwardTest(400);// test going forward for Test durration
+  // uhappyPlace();         // rock back and forth every x millis
+  // backAndForth();
+  otherHappyPlace();
 }
 
 void loop() {          // put your main code here, to run repeatedly:
@@ -28,3 +40,45 @@ void loop() {          // put your main code here, to run repeatedly:
 
 // ---- Your own functions ---- 
 
+void forwardTest(int durration){ // the folowing 4 instructions can be replaced w/ speedsSet()
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+  leftMotor->setSpeed(MAX_POWER);          // fullspeed left
+  rightMotor->setSpeed(MAX_POWER);         // fullspeed right
+  timer.setTimeout(stopMotors, durration); // stop the motor in 5 seconds (5000 milliseconds)
+}
+
+void backAndForth(){
+  setPower(MAX_POWER/2, MAX_POWER/2);
+  setDirections(FORWARD, FORWARD);
+  delay(100);
+  setPower(MAX_POWER/2, MAX_POWER/2);
+  setDirections(BACKWARD, BACKWARD);
+  delay(100);
+  stopMotors();
+}
+
+void happyPlace(){
+  static bool toggle = true;
+
+  setDirections(RELEASE, RELEASE);
+  if(toggle){
+    speedsSet(MAX_POWER/2, MAX_POWER/2);   // go forwards
+  } else {
+    speedsSet(-MAX_POWER/2, -MAX_POWER/2); // go backwards
+  }
+  toggle = !toggle;                    // flip toggle to opposite of toggle
+  timer.setTimeout(happyPlace, TEST_DURRATION); // call yourself from within yourself (recursion)
+}
+
+void otherHappyPlace(){
+  static bool toggle = true;
+
+  if(toggle){
+    setDirections(FORWARD, BACKWARD); // go forwards
+  } else {
+    setDirections(BACKWARD, FORWARD); // go backwards
+  }
+  toggle = !toggle;                    // flip toggle to opposite of toggle
+  timer.setTimeout(otherHappyPlace, TEST_DURRATION); // call yourself from within yourself (recursion)
+}
--- motors.ino
+++ motors.ino
@@ -1,22 +1,7 @@
 // motor.ino ~ Copyright 2016 Paul Beaudet ~ MIT license
-#include <Adafruit_MotorShield.h> // https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
-
-#define LEFT_MOTOR_PORT 3  // motor number on Adafruit shield
-#define RIGHT_MOTOR_PORT 4
-Adafruit_MotorShield motorShield = Adafruit_MotorShield(); // Create motor shield object w/ default I2C address
-Adafruit_DCMotor *leftMotor = motorShield.getMotor(LEFT_MOTOR_PORT);
-Adafruit_DCMotor *rightMotor = motorShield.getMotor(RIGHT_MOTOR_PORT);
 
 void motorSetup(){  
   motorShield.begin(); // start motors w/ default frequency 1.6KHz
-}
-
-void forwardTest(){
-  leftMotor->run(FORWARD);
-  rightMotor->run(FORWARD);
-  leftMotor->setSpeed(MAX_POWER);     // fullspeed left
-  rightMotor->setSpeed(MAX_POWER);    // fullspeed right
-  timer.setTimeout(stopMotors, 5000); // stop the motor in 5 seconds (5000 milliseconds)
 }
 
 void stopMotors(){
@@ -24,33 +9,6 @@ void stopMotors(){
   rightMotor->run(RELEASE); //
   leftMotor->setSpeed(0);   // stop right motor
   rightMotor->setSpeed(0);  // stop left motor
-}
-
-
-void speedsSet(byte leftPwr, byte rightPwr){
-  byte rightDir = BACKWARD;
-  byte leftDir = BACKWARD;
-  if(leftPwr){             // forward case
-    leftDir = FORWARD;
-  } else if (leftPwr < 0){ // backward case
-    leftPwr = abs(leftPwr);
-  } else {                 // release case (brake would also be possible)
-    leftDir = RELEASE;
-    // leftDir = BRAKE;
-  }
-  if(rightPwr){
-    rightDir = FORWARD;
-  } else if (rightPwr < 0){ // keep in mind backwards is default
-    rightPwr = abs(rightPwr);
-  } else {
-    rightDir = RELEASE;
-    // rightDir = BREAK;
-  }
-
-  leftMotor->setSpeed(leftPwr);
-  leftMotor->run(leftDir);
-  rightMotor->setSpeed(rightPwr);
-  rightMotor->run(rightDir);
 }
 
 void setDirections(byte left, byte right){
@@ -66,6 +24,31 @@ void setPower(byte left, byte right){
 void releaseMotors(){
   leftMotor->run(RELEASE);
   rightMotor->run(RELEASE);
+}
+
+void speedsSet(int leftPwr, int rightPwr){
+  byte rightDirection = BACKWARD;
+  byte leftDirection = BACKWARD;
+  // Serial.println(leftPwr);
+  if (leftPwr < 0){         // backward case
+    leftPwr = abs(leftPwr);
+  } else if(leftPwr > 0){   // forward case
+    leftDirection = FORWARD;
+  } else {                  // release case (brake would also be possible)
+    rightPwr = 0;
+    leftDirection = RELEASE;
+  }
+  if(rightPwr < 0){ // keep in mind backwards is default
+    rightPwr = abs(rightPwr);
+  } else if (rightPwr > 0){
+    rightDirection = FORWARD;
+  } else {
+    rightPwr = 0;
+    rightDirection = RELEASE;
+  }
+  // Serial.println(leftDirection);
+  setDirections(leftDirection, rightDirection);
+  setPower(leftPwr, rightPwr);
 }
 
 int speedPower(char mode){
